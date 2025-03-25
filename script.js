0a56fb0efc89acfe1242d8ea6b4cbe648a88bbd4
thanhthuy12a <nguyenhuynhthanhthuy1910@gmail.com>
2025-03-25 09:42:33 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+let a = document.getElementByld("username") .value;
+let b = document.getflementById("password") .value;
+if (a === "ththuy"
+&&b === *0709") {
+window.open ("home.html");
+document.getElenentById("compiler").style.display = "block";
+} else {
+document.getelementById("error-message").innerText="Không hợp lệ “;}
+}
